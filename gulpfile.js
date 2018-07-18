a37209f7f3f9adddebabc33b222f83edc1932d15
TAKTY-MBP-ST\Takty <tokutadaginaya@gmail.com>
2018-07-19 08:57:19 +0900

    Update packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,72 +1,77 @@
-var gulp       = require('gulp');
-var plumber    = require('gulp-plumber');
-var changed    = require('gulp-changed');
-var uglify     = require('gulp-uglify');
-var rename     = require('gulp-rename');
-var sourcemaps = require('gulp-sourcemaps');
-var sass       = require('gulp-sass');
-var cleanCSS   = require('gulp-clean-css');
-var babel      = require('gulp-babel');
+'use strict';
 
-gulp.task('js', function() {
-	gulp.src(['src/**/*.js', '!src/**/*.min.js', '!src/_backup/**/*'], {base: 'src'})
-	.pipe(plumber())
-	.pipe(babel({presets: ['es2015']}))
-	.pipe(uglify())
-	.pipe(rename({extname: '.min.js'}))
-	.pipe(gulp.dest('dist'));
+const gulp = require('gulp');
+const $ = require('gulp-load-plugins')({pattern: ['gulp-*']});
 
-	gulp.src(['src/**/*.min.js'])
-	.pipe(plumber())
-	.pipe(gulp.dest('dist'));
+
+gulp.task('js-raw', () => {
+	return gulp.src(['src/**/*.js', '!src/**/*.min.js', '!src/_backup/**/*'], {base: 'src'})
+		.pipe($.plumber())
+		.pipe($.babel({presets: [['env', {targets: {ie: 11}}]]}))
+		.pipe($.rename({extname: '.min.js'}))
+		.pipe($.uglify())
+		.pipe(gulp.dest('dist'));
 });
 
-gulp.task('sass', function () {
-	gulp.src(['src/**/*.scss', '!src/_backup/**/*'])
-	.pipe(plumber())
-    .pipe(sourcemaps.init())
-	.pipe(sass())
-	.pipe(cleanCSS())
-	.pipe(rename({extname: '.min.css'}))
-	.pipe(sourcemaps.write('.'))
-	.pipe(gulp.dest('dist'));
+gulp.task('js-min', () => {
+	return gulp.src(['src/**/*.min.js'])
+		.pipe($.plumber())
+		.pipe(gulp.dest('dist'));
 });
 
-gulp.task('css', function () {
-	gulp.src(['src/**/*.css', '!src/**/*.min.css', '!src/_backup/**/*'], {base: 'src'})
-	.pipe(plumber())
-    .pipe(sourcemaps.init())
-	.pipe(cleanCSS())
-	.pipe(rename({extname: '.min.css'}))
-	.pipe(sourcemaps.write('.'))
-	.pipe(gulp.dest('dist'));
+gulp.task('js', gulp.parallel('js-raw', 'js-min'));
 
-	gulp.src(['src/**/*.min.css'])
-	.pipe(plumber())
-	.pipe(gulp.dest('dist'));
+gulp.task('sass', () => {
+	return gulp.src(['src/**/*.scss', '!src/_backup/**/*'])
+		.pipe($.plumber())
+		.pipe($.sourcemaps.init())
+		.pipe($.sass({outputStyle: 'compressed'}))
+		.pipe($.autoprefixer({browsers: ['ie >= 11'], remove: false}))
+		.pipe($.rename({extname: '.min.css'}))
+		.pipe($.sourcemaps.write('.'))
+		.pipe(gulp.dest('dist'));
 });
 
-gulp.task('php', function() {
-	gulp.src(['src/**/*.php', '!src/_backup/**/*'])
-	.pipe(changed('dist'))
-	.pipe(plumber())
-	.pipe(gulp.dest('dist'));
+gulp.task('css-raw', () => {
+	return gulp.src(['src/**/*.css', '!src/**/*.min.css', '!src/_backup/**/*'], {base: 'src'})
+		.pipe($.plumber())
+		.pipe($.sourcemaps.init())
+		.pipe($.cleanCss())
+		.pipe($.rename({extname: '.min.css'}))
+		.pipe($.sourcemaps.write('.'))
+		.pipe(gulp.dest('dist'));
 });
 
-gulp.task('img', function() {
-	gulp.src(['src/**/*.png', 'src/**/*.jpg', 'src/**/*.jpeg', 'src/**/*.svg', '!src/_backup/**/*'], {base: 'src'})
-	.pipe(changed('dist'))
-	.pipe(plumber())
-	.pipe(gulp.dest('dist'));
+gulp.task('css-min', () => {
+	return gulp.src(['src/**/*.min.css'])
+		.pipe($.plumber())
+		.pipe(gulp.dest('dist'));
 });
 
-gulp.task('watch', function() {
-	gulp.watch('src/**/*.js', ['js']);
-	gulp.watch('src/**/*.scss', ['sass']);
-	gulp.watch('src/**/*.css', ['css']);
-	gulp.watch('src/**/*.php', ['php']);
-	gulp.watch('src/**/*.img', ['img']);
+gulp.task('css', gulp.parallel('css-raw', 'css-min'));
+
+gulp.task('php', () => {
+	return gulp.src(['src/**/*.php', '!src/_backup/**/*'])
+		.pipe($.changed('dist'))
+		.pipe($.plumber())
+		.pipe(gulp.dest('dist'));
 });
 
-gulp.task('build', ['js', 'sass', 'css', 'php', 'img']);
-gulp.task('default', ['js', 'sass', 'css', 'php', 'img', 'watch']);
+gulp.task('img', () => {
+	return gulp.src(['src/**/*.png', 'src/**/*.jpg', 'src/**/*.jpeg', 'src/**/*.svg', '!src/_backup/**/*'], {base: 'src'})
+		.pipe($.changed('dist'))
+		.pipe($.plumber())
+		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', () => {
+	gulp.watch('src/**/*.js', gulp.series('js'));
+	gulp.watch('src/**/*.scss', gulp.series('sass'));
+	gulp.watch('src/**/*.css', gulp.series('css'));
+	gulp.watch('src/**/*.php', gulp.series('php'));
+	gulp.watch('src/**/*.img', gulp.series('img'));
+});
+
+gulp.task('build', gulp.parallel('js', 'sass', 'css', 'php', 'img'));
+
+gulp.task('default', gulp.series('build', 'watch'));
